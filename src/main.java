9df7a2748279446eac0ca157e7210b182a39e43b
SELAHATTİN <SELAHATTN@TOSHIBA>
2017-05-02 21:01:55 +0300

    file modified.

--- src/main.java
+++ src/main.java
@@ -10,15 +10,15 @@ public class main {
 			rsaObject.generateKey();
 		
 		/*txt dosyasýnýn bulunduðu klasöre gider ve dosyayý okur.*/
-		String plainText = rsaObject.readData("CryptoFiles/plainText.txt");
+		String plainText = rsaObject.readData("C:\\Users\\USER\\Desktop\\Projeler\\RsaCryptTxt\\CrytoFiles\\plainText.txt");
 		System.out.println("Sifrelenecek Metin: " + plainText);
 		
 		/*Dosyadan okunan mesajý þifreleme iþlemine sokar ve þifreli mesajý ekrana yazdýrýr.*/
-		String result = rsaObject.encrypt("CryptoFiles/plainText.txt", "CryptoFiles/cipherText.txt");
+		String result = rsaObject.encrypt("C:\\Users\\USER\\Desktop\\Projeler\\RsaCryptTxt\\CrytoFiles\\plainText.txt", "C:\\Users\\USER\\Desktop\\Projeler\\RsaCryptTxt\\CrytoFiles\\cipherText.txt");
 		System.out.println(result);
 		
 		/*Þifreli mesajý þifre çözme iþlemine sokar ve çözülmüþ mesajý ekrana yazdýrýr.*/
-		result = rsaObject.decrypt("CryptoFiles/cipherText.txt", "CryptoFiles/plainTextAgain.txt");
+		result = rsaObject.decrypt("C:\\Users\\USER\\Desktop\\Projeler\\RsaCryptTxt\\CrytoFiles\\cipherText.txt", "C:\\Users\\USER\\Desktop\\Projeler\\RsaCryptTxt\\CrytoFiles\\plainTextAgain.txt");
 		System.out.println(result);
 	}
 
